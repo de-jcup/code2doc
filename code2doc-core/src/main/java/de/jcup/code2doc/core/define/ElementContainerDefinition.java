6d1af29f486f1a2ba093c65aec9c8fd523a27974
Albert Tregnaghi <albert.tregnaghi@gmail.com>
2015-04-24 16:04:40 +0200

    added java doc to element container definition

--- code2doc-core/src/main/java/de/jcup/code2doc/core/define/ElementContainerDefinition.java
+++ code2doc-core/src/main/java/de/jcup/code2doc/core/define/ElementContainerDefinition.java
@@ -32,31 +32,73 @@ import de.jcup.code2doc.api.UseCase;
 public interface ElementContainerDefinition<T> extends Definition{
 
 	/**
-	 * Add a use case to this container
+	 * Add a use case to this container - if already added the add will be ignored
 	 * @param useCase - use case class
 	 * @return container
 	 */
 	public T addUseCase(Class<? extends UseCase> useCase);
 	
 	/**
-	 * Adds a use case and starts defining use case in detail
-	 * @param useCase
-	 * @return
+	 * Adds a use case - if already added the former found will be reused - and define additional use case details
+	 * @param useCase - use case class
+	 * @return use case definition
 	 */
 	public UseCaseDefinition addUseCaseAndDefine(Class<? extends UseCase> useCase);
 	
-	
+	/**
+	 * Add an architecture to this container - if already added the add will be ignored
+	 * @param architecture - architecture class
+	 * @return container
+	 */
 	public T addArchitecture(Class<? extends Architecture> architecture);
+	
+	/**
+	 * Adds an architecture  - if already added the former found will be reused - and define additional architecture details
+	 * @param architecture - architecture class
+	 * @return architecture definition
+	 */
 	public ArchitectureDefinition addArchitectureAndDefine(Class<? extends Architecture> architecture);
 	
+	/**
+	 * Add a concept to this container - if already added the add will be ignored
+	 * @param concept - concept class
+	 * @return container
+	 */
 	public T addConcept(Class<? extends Concept> concept);
+	
+	/**
+	 * Adds a concept  - if already added the former found will be reused - and define additional concept details
+	 * @param concept - concept class
+	 * @return concept definition
+	 */
 	public ConceptDefinition addConceptAndDefine(Class<? extends Concept> concept);
 	
-
+	/**
+	 * Add a constraint to this container - if already added the add will be ignored
+	 * @param constraint - constraint class
+	 * @return container
+	 */
 	public T addConstraint(Class<? extends Constraint> concept);
+	
+	/**
+	 * Adds a constraint  - if already added the former found will be reused - and define additional concept details
+	 * @param constraint - constraint class
+	 * @return constraint definition
+	 */
 	public ConstraintDefinition addConstraintAndDefine(Class<? extends Constraint> concept);
 	
+	/**
+	 * Add a constraint to this container - if already added the add will be ignored
+	 * @param concept - concept class
+	 * @return container 
+	 */
 	public T addRole(Class<? extends Role> concept);
+	
+	/**
+	 * Adds a role  - if already added the former found will be reused - and define additional role details
+	 * @param role - role class
+	 * @return role definition
+	 */
 	public RoleDefinition addRoleAndDefine(Class<? extends Role> concept);
 	
 }
